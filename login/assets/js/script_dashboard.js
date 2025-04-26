23d212bb4fe27ece298324c74d25b1d42b7cc8c9
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2025-04-26 16:13:22 -0400

    script modified

--- login/assets/js/script_dashboard.js
+++ login/assets/js/script_dashboard.js
@@ -1,6 +1,3 @@
-// const token = localStorage.getItem('auth_token');
-document.getElementById('token-display').textContent = token;
-
 function logout() {
     localStorage.removeItem('auth_token');
     window.location.replace('/');
