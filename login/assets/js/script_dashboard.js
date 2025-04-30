8f94f91a4be64215bc3a21d8ebaac67cfeaa663c
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2025-04-30 08:45:16 -0400

    some clean

--- login/assets/js/script_dashboard.js
+++ login/assets/js/script_dashboard.js
@@ -1,18 +1,19 @@
 function logout() {
-    localStorage.removeItem('auth_token');
-    localStorage.clear();
+  showToast('success', 'You have been logged out successfully!');
+  localStorage.removeItem('auth_token');
+  localStorage.removeItem('user_info');
+  localStorage.removeItem('email'); 
+  setTimeout(() => {
     window.location.replace('/login/index.html');
+  }, 100);
   }
 
   document.addEventListener('DOMContentLoaded', () => {
     const userInfo = localStorage.getItem('user_info');
-    
     if (userInfo) {
       const user = JSON.parse(userInfo);
       document.getElementById('welcome-message').innerHTML = `Welcome, ${user.username}! <br> ${user.email}`;
-
     } else {
-      // If no user info, redirect to login page
       window.location.href = '/login/index.html';
     }
   });
