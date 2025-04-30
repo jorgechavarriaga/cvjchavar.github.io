d759cfb483c867256538ef1c6252f7f33bc74d26
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2025-04-30 19:52:36 -0400

    swagger

--- login/assets/js/script_dashboard.js
+++ login/assets/js/script_dashboard.js
@@ -1,3 +1,26 @@
+function swaggerDocumentation(){
+  const token = localStorage.getItem('auth_token');
+  if (!token) {
+    showToast('error', 'Token not valid.')
+    window.location.href = "/login/index.html"; 
+    exit;
+  } else {
+    showToast('success', 'ChavaZystem.tech API Docs');
+    window.location.href = "https://api.chavazystem.tech/api-docs"; 
+  }
+};
+
+function getURLParameter(name) {
+  return new URLSearchParams(window.location.search).get(name);
+}
+
+window.addEventListener('load', () => {
+  const error = getURLParameter('error');
+  if (error === 'rate_limit_docs') {
+    showToast('error', 'Too many requests to documentation');
+  }
+});
+
 function logout() {
   showToast('success', 'You have been logged out successfully!');
   localStorage.removeItem('auth_token');
@@ -6,7 +29,7 @@ function logout() {
   setTimeout(() => {
     window.location.replace('/login/index.html');
   }, 100);
-  }
+  };
 
   document.addEventListener('DOMContentLoaded', () => {
     const userInfo = localStorage.getItem('user_info');
